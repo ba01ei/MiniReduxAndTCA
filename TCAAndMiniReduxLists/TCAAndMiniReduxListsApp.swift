73093a84739878437fcc2a2e9bdbb19a4e4d2c28
Bao Lei <bao@lei.fyi>
2026-05-18 13:33:23 -0700

    Initial Commit

---
+++ TCAAndMiniReduxLists/TCAAndMiniReduxListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAAndMiniReduxListsApp.swift
+//  TCAAndMiniReduxLists
+//
+//  Created by Bao Lei on 5/18/26.
+//
+
+import SwiftUI
+
+@main
+struct TCAAndMiniReduxListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
